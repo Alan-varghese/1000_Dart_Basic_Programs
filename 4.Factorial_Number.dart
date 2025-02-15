2fcf4a1114ec319498e42ea201f82ff6561bfb8f
Alan-varghese <alanvz2212@gmail.com>
2025-02-16 02:42:31 +0400

    Updated

--- 4.Factorial_Number.dart
+++ 4.Factorial_Number.dart
@@ -1,10 +1,9 @@
-
 int factorial(int n) {
   if (n == 0) return 1;
   return n * factorial(n - 1);
 }
 
 void main() {
-  int num = 4;
-  print("$num The Factorial of the number is ${factorial(num)}");
+  int num = 5;
+  print("$num The Factorial is ${factorial(num)}");
 }
